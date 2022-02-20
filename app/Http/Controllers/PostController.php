3ca7ba2f36a13d562c70b6a3705c5c4124a0cc69
oobahiroto <shukatsupex@gmail.com>
2022-02-20 19:12:55 +0900

    saisyonokomiitto

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return view('posts/index')->with(['posts' => $post->getByLimit()]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('posts/create');
+    }
+
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit()
+    {
+        return '成功';
+    }
+ 
+}
