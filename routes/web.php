7838ed4ffda17cb514af1d9a4949db7916966405
oobahiroto <shukatsupex@gmail.com>
2022-02-21 19:26:51 +0900

    first commit2

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@
 Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
+Route::get('/posts/{post}/edit','PostController@edit');
+Route::put('/posts/{post}','PostController@update');
 Route::post('/posts/store', 'PostController@store');
-Route::get('/a','PostController@edit');
